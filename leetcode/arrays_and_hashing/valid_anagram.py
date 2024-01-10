7e3bf5bd53d490f98ba93781bc83c717281b434f
Artemiy Kokhanov <artemkokhanov98@gmail.com>
2024-01-10 11:31:02 -0500

    a

--- leetcode/arrays_and_hashing/valid_anagram.py
+++ leetcode/arrays_and_hashing/valid_anagram.py
@@ -5,7 +5,6 @@ def isAnagram(s: str, t: str) -> bool:
     countS = {}
     countT = {}
     for i in range(len(s)):
-        print(i)
         countS[s[i]] = 1 + countS.get(s[i], 0)
         countT[t[i]] = 1 + countT.get(t[i], 0)
 
