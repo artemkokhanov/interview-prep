1f9c14cceb7a1482dd8def98223e0477748f237c
Artemiy Kokhanov <artemkokhanov98@gmail.com>
2025-02-10 13:24:24 -0500

    Reviewing

--- dsa_for_beginners_neetcode/linked_lists/singly_linked_lists/reverse_linked_list.py
+++ dsa_for_beginners_neetcode/linked_lists/singly_linked_lists/reverse_linked_list.py
@@ -1,37 +1,37 @@
-# This is a two-pointer solution
 from typing import Optional
 
 
-# Definition for singly-linked list.
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self, val=0, nxt=None):
         self.val = val
-        self.next = next
+        self.nxt = nxt
 
 
-# iterative solution has time complexity O(n) and space complexity O(1)
-class iterativeSolution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, curr = None, head
+# time complexity O(n)
+# space complexity O(1)
+def reverseListTwoPointer(head: Optional[ListNode]) -> Optional[ListNode]:
+    prev, curr = None, head
 
-        while curr:
-            nxt = curr.next  # store the link to the next node
-            curr.next = prev  # reversing the link to the next node
-            prev = curr  # shifting prev to current node
-            curr = nxt  # shifting current node to next node to reverse
-        return prev  # returning new head
+    while curr:
+        nxt = curr.next  # store the link to the next node
+
+        curr.next = prev  # reversing the link to the next node
+        prev = curr  # shifting prev to current node
+
+        curr = nxt  # shifting current node to next node to reverse
+    return prev  # returning new head
 
 
-# recursive solution has time complexity O(n) and space complexity O(1)
-class recursiveSolution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:  # base case
-            return None
+# time complexity O(n)
+# space complexity O(1)
+def reverseListRecursively(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    if not head:  # base case
+        return None
 
-        newHead = head  # current node we are at with recursive call
-        if head.next:  # "if there is still a sub-problem"
-            newHead = self.reverseList(head.next)
-            head.next.next = head  # reversing the link between the next node and head
-        head.next = None
+    new_head = head  # current node we are at with recursive call
+    if head.next:  # "if there is still a sub-problem"
+        new_head = self.reverseList(head.next)
+        head.next.next = head  # reversing the link between the node in front and the current node
+    head.next = None  # set the current nodes next node to null
 
-        return newHead
+    return new_head
