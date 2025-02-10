2b4399deb5e89077f61f589ef110a2b6807f040a
Artemiy Kokhanov <artemkokhanov98@gmail.com>
2025-02-10 13:54:48 -0500

    Reviewing

--- dsa_for_beginners_neetcode/linked_lists/singly_linked_lists/reverse_linked_list.py
+++ dsa_for_beginners_neetcode/linked_lists/singly_linked_lists/reverse_linked_list.py
@@ -7,15 +7,17 @@ class ListNode:
         self.nxt = nxt
 
 
+# https://youtu.be/G0_I-ZF0S38
+
 # time complexity O(n)
 # space complexity O(1)
 def reverseListTwoPointer(head: Optional[ListNode]) -> Optional[ListNode]:
     prev, curr = None, head
 
     while curr:
-        nxt = curr.next  # store the link to the next node
+        nxt = curr.nxt  # store the link to the next node
 
-        curr.next = prev  # reversing the link to the next node
+        curr.nxt = prev  # reversing the link to the next node
         prev = curr  # shifting prev to current node
 
         curr = nxt  # shifting current node to next node to reverse
@@ -24,13 +26,13 @@ def reverseListTwoPointer(head: Optional[ListNode]) -> Optional[ListNode]:
 
 # time complexity O(n)
 # space complexity O(1)
-def reverseListRecursively(self, head: Optional[ListNode]) -> Optional[ListNode]:
+def reverseListRecursively(head: Optional[ListNode]) -> Optional[ListNode]:
     if not head:  # base case
         return None
 
     new_head = head  # current node we are at with recursive call
     if head.next:  # "if there is still a sub-problem"
-        new_head = self.reverseList(head.next)
+        new_head = reverseListRecursively(head.next)
         head.next.next = head  # reversing the link between the node in front and the current node
     head.next = None  # set the current nodes next node to null
 
