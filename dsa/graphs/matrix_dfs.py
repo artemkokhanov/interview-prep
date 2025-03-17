72a23da837f5bbbe21ce0070e8f2a721750c6dc9
Artemiy Kokhanov <artemkokhanov98@gmail.com>
2025-03-17 15:20:12 -0400

    matrix dfs

--- dsa/graphs/matrix_dfs.py
+++ dsa/graphs/matrix_dfs.py
@@ -1,3 +1,10 @@
+# Matrix (2D Grid)
+grid = [[0, 0, 0, 0],
+        [1, 1, 0, 0],
+        [0, 0, 0, 1],
+        [0, 1, 0, 0]]
+
+
 # Count paths (backtracking)
 def dfs(grid, r, c, visit):
     ROWS, COLS = len(grid), len(grid[0])
@@ -8,3 +15,17 @@ def dfs(grid, r, c, visit):
         return 0
     if r == ROWS - 1 and c == COLS - 1:
         return 1
+
+    visit.add((r, c))
+
+    count = 0  # count keeps track of how many different ways we can reach the result from the current location
+    count += dfs(grid, r + 1, c, visit)
+    count += dfs(grid, r - 1, c, visit)
+    count += dfs(grid, r - 1, c + 1, visit)
+    count += dfs(grid, r - 1, c - 1, visit)
+
+    visit.remove((r, c))
+    return count  # we return the count to the previous node/vertex which will return its count to its previous node/vertex, etc.
+
+
+print(dfs(grid, 0, 0, set()))
