e0a3ba6f15a87fe847644fd71dde9a5e8f9b55bb
Artemiy Kokhanov <artemkokhanov98@gmail.com>
2025-03-23 14:48:05 -0400

    leetcoding

---
+++ leetcode/sliding_window/longest_substring_without_repeating_characters.py
@@ -0,0 +1,12 @@
+def lengthOfLongestSubstring(s: str) -> int:
+    charSet = set()
+    L = 0
+    result = 0
+
+    for R in range(len(s)):
+        while s[R] in charSet:
+            charSet.remove(s[L])
+            L += 1
+        charSet.add(s[R])
+        result = max(result, R - L + 1)
+    return result
