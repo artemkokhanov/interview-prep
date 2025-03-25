20853873cdbae83920122d199608fe2ea6a4e847
Artemiy Kokhanov <artemkokhanov98@gmail.com>
2025-03-25 17:46:33 -0400

    number of islands

---
+++ leetcode/graphs/number_of_islands.py
@@ -0,0 +1,33 @@
+from collections import deque
+from typing import List
+
+
+def numIslands(grid: List[List[str]]) -> int:
+    if not grid:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    visit = set()
+    islands = 0
+
+    def bfs(r, c):
+        queue = deque()
+        visit.add((r, c))
+        queue.append((r, c))
+
+        while queue:
+            r, c = queue.popleft()
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+            for dr, dc in directions:
+                if (r + dr) in range(rows) and (c + dc) in range(cols) and grid[r + dr][c + dc] == "1" and (
+                        (r + dr, c + dc)) not in visit:
+                    queue.append((r + dr, c + dc))
+                    visit.add((r + dr, c + dc))
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == "1" and (r, c) not in visit:
+                bfs(r, c)
+                islands += 1
+    return islands
