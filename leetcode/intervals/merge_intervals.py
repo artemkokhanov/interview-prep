cb70efd18c0389844142aaddead0f9245623078e
Artemiy Kokhanov <artemkokhanov98@gmail.com>
2025-03-29 18:47:04 -0400

    merge intervals

--- leetcode/intervals/merge_intervals.py
+++ leetcode/intervals/merge_intervals.py
@@ -4,13 +4,13 @@ from typing import List
 def merge(intervals: List[List[int]]) -> List[List[int]]:
     # O(nlogn) solution where n is the number of intervals we are given because we are first sorting then iterating
     intervals.sort(key=lambda i: i[0])  # sorting by the start values of each interval
-    output = [intervals[0]]  # avoiding edge case
+    result = [intervals[0]]  # avoiding edge case
 
     for start, end in intervals[1:]:
-        lastEnd = output[-1][1]
+        lastEnd = result[-1][1]
 
         if start <= lastEnd:
-            output[-1][1] = max(lastEnd, end)
+            result[-1][1] = max(lastEnd, end)
         else:
-            output.append([start, end])
-    return output
+            result.append([start, end])
+    return result
