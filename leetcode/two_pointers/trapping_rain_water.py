330c1ce8ff98d935fd187ae055fe0dacdbfa9e5f
Artemiy Kokhanov <artemkokhanov98@gmail.com>
2025-04-01 15:00:08 -0400

    trapping rain water

---
+++ leetcode/two_pointers/trapping_rain_water.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+def trap(height: List[int]) -> int:
+    if not height: return 0
+
+    l, r = 0, len(height) - 1
+    leftMax, rightMax = height[l], height[r]
+    res = 0
+
+    while l < r:
+        if leftMax < rightMax:
+            l += 1
+            leftMax = max(leftMax, height[l])
+            res += leftMax - height[l]  # this will never be negative because we are updating leftMax before
+        else:
+            r -= 1
+            rightMax = max(rightMax, height[r])
+            res += rightMax - height[r]
+    return res
