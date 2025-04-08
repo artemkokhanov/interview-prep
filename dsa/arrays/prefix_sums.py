e135ea7cd48a74bcd673a163c74d1b211c6ad390
Artemiy Kokhanov <artemkokhanov98@gmail.com>
2025-04-08 19:59:25 -0400

    prefix sums

--- dsa/arrays/prefix_sums.py
+++ dsa/arrays/prefix_sums.py
@@ -1,6 +1,7 @@
 class PrefixSum:
 
     def __init__(self, nums):
+        # O(n)
         self.prefix = []
         total = 0
         for n in nums:
@@ -8,6 +9,7 @@ class PrefixSum:
             self.prefix.append(total)
 
     def rangeSum(self, left, right):
+        # O(1)
         preRight = self.prefix[right]
         preLeft = self.prefix[left - 1] if left > 0 else 0
         return preRight - preLeft
