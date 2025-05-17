6de6669c62e92e34b670cfc4cfc4351c09bdcf64
Artemiy Kokhanov <artemkokhanov98@gmail.com>
2025-05-17 14:50:21 -0400

    review

--- leetcode/1-d_dp/longest_palindromic_substring.py
+++ leetcode/1-d_dp/longest_palindromic_substring.py
@@ -2,7 +2,9 @@ def longest_palindrome(s: str) -> str:
     res = ''
     res_length = 0
 
+    # Time Complexity: O(n^2) because for each character in the string, we expand left and right for that character, so n * n which is n^2)
     for i in range(len(s)):
+        # for odd palindromes (odd because we take one character and expand to the left and right which will always give us an odd length):
         left, right = i, i
         while left >= 0 and right < len(s) and s[left] == s[right]:
             if (right - left + 1) > res_length:
@@ -11,6 +13,7 @@ def longest_palindrome(s: str) -> str:
             left -= 1
             right += 1
 
+        # for even palindromes:
         left, right = i, i + 1
         while left >= 0 and right < len(s) and s[left] == s[right]:
             if (right - left + 1) > res_length:
