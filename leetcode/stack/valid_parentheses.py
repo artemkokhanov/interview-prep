cddbe7338a45b79d3547f3bbdc9aede5e922ccc9
Artemiy Kokhanov <artemkokhanov98@gmail.com>
2025-05-17 13:40:32 -0400

    review

--- leetcode/stack/valid_parentheses.py
+++ leetcode/stack/valid_parentheses.py
@@ -4,7 +4,7 @@ def is_valid(s: str) -> bool:
 
     for c in s:
         if c in closeToOpen:  # in checks the keys not the values
-            if stack and stack[-1] == closeToOpen[c]:
+            if stack and stack[-1] == closeToOpen[c]:  # stack[-1] is the top of the stack
                 stack.pop()
             else:
                 return False
